9ddc8750143a711a1cae6b4fbf2c80bd709481c3
kahd0 <v.rikardo@gmail.com>
2024-02-29 09:02:50 -0300

    m01s06 - 01

---
+++ M1S01/PaginaPessoal/assets/js/m01e04.js
@@ -0,0 +1,52 @@
+// exercício 01
+
+function exibirMensagem() {
+    console.log("Olá, Floripa Mais Tec!");
+}
+
+// exercício 02
+
+let numero = window.prompt("Por favor, insira um número:");
+console.log("O número inserido é:", numero);
+
+// exercício 03
+
+
+let str_nome = "Ricardo";
+let str_sobrenome = "Vieira";
+let str_nomeCompleto = str_nome + " " + str_sobrenome;
+
+console.log(str_nomeCompleto);
+
+// exercício 04
+
+function somarNumeros(num1, num2) {
+    let soma = num1 + num2;
+    console.log("A soma dos números é:", soma);
+}
+
+// exercício 05
+
+function verificarNumero(numero) {
+    if (numero > 0) {
+        console.log(numero + " é positivo.");
+    } else if (numero < 0) {
+        console.log(numero + " é negativo.");
+    } else {
+        console.log("O número é zero.");
+    }
+}
+
+// exercício 06
+
+
+// exercício 07
+
+
+// exercício 08
+
+
+// exercício 09
+
+
+// exercício 10
